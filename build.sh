f4b76f67a3795378703b976821fac6efba485737
Dean <dean.he90@gmail.com>
2025-06-08 23:43:58 +0200

    modify build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@
 echo "Building the Docker image..."
 docker build -t $(basename "$(pwd)") -f Dockerfile .
 
-# Run the container with bind mount
-#echo "Running the container..."
-docker run -it --mount type=bind,source="$(pwd)",target=/workspace $(basename "$(pwd)") /bin/bash
